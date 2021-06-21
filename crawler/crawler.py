25ea8c7b964bf8d907c70e80e8c98b811ebf2676
Ubuntu <ubuntu@ip-172-31-0-244.ap-northeast-2.compute.internal>
2021-06-21 03:27:31 +0000

    conn_strapi

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -5,8 +5,14 @@ import json
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('DeepSearch'))))
-from connector import es_connector
+from connector import es_connector,strapi_connector
+import requests
+import json
 
+# 검색 키워드 
+strapi = strapi_connector.Strapi()
+keywords_db = strapi.get_db()
+search_keywords = [K['Keywords'] for K in keywords_db]
 
 #chrome driver
 chrome_options = webdriver.ChromeOptions()
@@ -15,28 +21,20 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome("chromedriver", options = chrome_options)
 
-
-#date
+# 기간 설정
 today = datetime.datetime.now()
 yesterday = today + datetime.timedelta(days=-1)
-
-
-# 검색 키워드 입력
-search_keywords = ['삼성생명','라이나생명','ESG','보험','금융']
-
-# 기간 설정
 years = [yesterday.strftime("%Y-%m-%d")[:4]]
 months = [datetime.datetime.now().strftime("%Y-%m-%d")[5:7]]
 all_days = [yesterday.strftime("%Y-%m-%d")[-2:]]
-
 article_url_df = get_article_df(years, months, all_days, search_keywords, driver)
 urls = article_url_df['url']
+
 #기사 내용 수집
 article_dicts = []
 reply_dfs = []
 
-
-
+#elasticsearch
 es = es_connector.ES()
 
 # with open('settings.json','r') as f:
@@ -51,7 +49,7 @@ for i in range(len(article_url_df)):
     try:
         article_dict = Get_Article_Body(article_url_df['url'][i],driver)
         article_dict['search_keywords'] = article_url_df['search_keyword'][i]
-        print(article_dict)
+        #print(article_dict)
         es.dataInsert(index='news-naver', data=article_dict)
         #reply_df = ariticle_reply()
         #reply_dfs.append(reply_df)
