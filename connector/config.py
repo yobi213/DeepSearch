b16796e3d80a36e37314fbf4cadb33569d66dbcf
yobi213 <yeop7747@gmail.com>
2021-06-25 14:21:40 +0900

    env_var

--- connector/config.py
+++ connector/config.py
@@ -1,8 +1,13 @@
+import os
+
 class es_config:
-    hosts = 'https://search-deepsearch-bztxmrzp24xb4ms6tddde7vypm.ap-northeast-2.es.amazonaws.com'
-    auth = ('deepsearch', 'Lina123$')
+#     hosts = 'https://search-deepsearch-bztxmrzp24xb4ms6tddde7vypm.ap-northeast-2.es.amazonaws.com'
+#     auth = ('deepsearch', 'Lina123$')
+    hosts = os.getenv('ES_HOSTS')
+    auth = os.getenv('ES_AUTH')
 
 
 class strapi_config:
-    hosts = 'http://13.124.235.216:1337/'
+    #hosts = 'http://13.124.235.216:1337/'
+    hosts = os.getenv('STRAPI_HOSTS')
     collection = 'crawler-keywords'
